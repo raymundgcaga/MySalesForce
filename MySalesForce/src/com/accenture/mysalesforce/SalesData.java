b632332f54faf6b500a1aa435fecd22260ee330c
raymund.g.caga <raymund.g.caga@.accenture.com>
2017-03-30 14:25:59 -0500

    Added 2 and 10 to the SalesData

--- MySalesForce/src/com/accenture/mysalesforce/SalesData.java
+++ MySalesForce/src/com/accenture/mysalesforce/SalesData.java
@@ -2,10 +2,11 @@ package com.accenture.mysalesforce;
 
 public class SalesData {
 
-	int data[] = {0, 4, 8};
+	int data[] = {0, 2, 4, 8, 10};
 	
 	public void display()
 	{
 		System.out.println("TODO: Implement SalesData.display()");
+		System.out.println("TODO: Added 2 and 10 in the list");
 	}
 }
